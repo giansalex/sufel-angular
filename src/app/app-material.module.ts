0406446ee1f4378454858fc52bdba486ae4df506
Giancarlos Salas <giansalex@gmail.com>
2017-08-27 16:46:53 -0500

    Fix Material style

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -3,37 +3,46 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
     MdButtonModule,
+    MdCardModule,
     MdCheckboxModule,
+    MdIconModule,
     MdInputModule,
     MdMenuModule,
     MdProgressBarModule,
     MdRadioModule,
     MdSidenavModule,
     MdSnackBarModule,
+    MdToolbarModule,
 } from '@angular/material';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     MdButtonModule,
+    MdCardModule,
     MdCheckboxModule,
+    MdIconModule,
     MdInputModule,
     MdMenuModule,
     MdProgressBarModule,
     MdRadioModule,
     MdSidenavModule,
     MdSnackBarModule,
+    MdToolbarModule,
   ],
   exports: [
     BrowserAnimationsModule,
     MdButtonModule,
+    MdCardModule,
     MdCheckboxModule,
+    MdIconModule,
     MdInputModule,
     MdMenuModule,
     MdProgressBarModule,
     MdRadioModule,
     MdSidenavModule,
     MdSnackBarModule,
+    MdToolbarModule,
   ]
 })
 export class AppMaterialModule { }
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -5,7 +5,7 @@ import { AuthService } from './../services';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  
+
   constructor(
     private router: Router,
     private auth: AuthService
@@ -14,11 +14,12 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.auth.isAuthorized()) {
       return true;
-    }
+    // if (this.auth.isAuthorized()) {
+    //   return true;
+    // }
 
-    this.router.navigate(['/login']);
-    return false;
+    // this.router.navigate(['/login']);
+    // return false;
   }
 }
